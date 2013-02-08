eb46b915ac94d84520c76a10b4245863c9d67560
noahlh <nlh@noahlh.com>
2013-02-08 17:56:22 -0500

    Update vendor/assets/javascripts/clear_eyes.js

--- vendor/assets/javascripts/clear_eyes.js
+++ vendor/assets/javascripts/clear_eyes.js
@@ -1,2 +1,9 @@
-var ratio = (window.devicePixelRatio !== undefined ? window.devicePixelRatio : 1);
-document.cookie = 'devicePixelRatio=' + ratio + (document.cookie != "" ? "; " + document.cookie : "");
+ratio = (window.devicePixelRatio !== undefined ? window.devicePixelRatio : 1);
+oldRatio = $.cookie('devicePixelRatio');
+
+if ((oldRatio !== undefined) && (oldRatio == ratio)) {
+}
+else {
+  $.cookie('devicePixelRatio', ratio);
+	window.location.reload();
+}
